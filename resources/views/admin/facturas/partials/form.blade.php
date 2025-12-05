2ac1e3926dbce976c43201d64110975fa72eac77
juanchehin <chehinjuan@gmail.com>
2025-12-05 11:25:04 -0300

    fix nombre recibo

--- resources/views/admin/facturas/partials/form.blade.php
+++ resources/views/admin/facturas/partials/form.blade.php
@@ -19,7 +19,7 @@
     {{-- Fecha de Recibo --}}
     <div class="col-md-4">
         <div class="form-group">
-            <label for="fecha_emision">Fecha Recibo</label>
+            <label for="fecha_emision">Fecha Remito</label>
             <input type="date" name="fecha_emision" id="fecha_emision"
                    value="{{ old('fecha_emision', date('Y-m-d')) }}"
                    class="form-control" required>
@@ -131,7 +131,7 @@
     <tr>
         <th style="width: 150px;">Pto. Venta</th>
         <th style="width: 180px;">Comprobante</th>
-        <th style="width: 180px;">Fecha Recibo</th>
+        <th style="width: 180px;">Fecha Remito</th>
         <th style="width: 60px;"></th>
     </tr>
     </thead>
