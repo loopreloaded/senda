5015d96562cfd95088b5a4dbf4e1b82571bc1871
juanchehin <chehinjuan@gmail.com>
2025-12-12 11:49:27 -0300

    converion unidad a texto

--- resources/views/admin/facturas/show.blade.php
+++ resources/views/admin/facturas/show.blade.php
@@ -10,6 +10,57 @@
     </div>
 @stop
 
+@php
+    $unidadTexto = [
+        1  => 'kilogramos',
+        2  => 'metros',
+        3  => 'metros cuadrados',
+        4  => 'metros cúbicos',
+        5  => 'litros',
+        6  => '1000 kWh',
+        7  => 'unidades',
+        8  => 'pares',
+        9  => 'docenas',
+        10 => 'quilates',
+        11 => 'millares',
+        14 => 'gramos',
+        15 => 'milímetros',
+        16 => 'mm cúbicos',
+        17 => 'kilómetros',
+        18 => 'hectolitros',
+        20 => 'centímetros',
+        25 => 'jgo. pqt. mazo naipes',
+        27 => 'cm cúbicos',
+        29 => 'toneladas',
+        30 => 'dam cúbicos',
+        31 => 'hm cúbicos',
+        32 => 'km cúbicos',
+        33 => 'microgramos',
+        34 => 'nanogramos',
+        35 => 'picogramos',
+        41 => 'miligramos',
+        47 => 'mililitros',
+        48 => 'curie',
+        49 => 'milicurie',
+        50 => 'microcurie',
+        51 => 'uiacthor',
+        52 => 'muiacthor',
+        53 => 'kg base',
+        54 => 'gruesa',
+        61 => 'kg bruto',
+        62 => 'uiactant',
+        63 => 'muiactant',
+        64 => 'uiactig',
+        65 => 'muiactig',
+        66 => 'kg activo',
+        67 => 'gramo activo',
+        68 => 'gramo base',
+        96 => 'packs',
+        98 => 'otras unidades',
+    ];
+@endphp
+
+
 @section('content')
 
     {{-- Mensajes de éxito --}}
