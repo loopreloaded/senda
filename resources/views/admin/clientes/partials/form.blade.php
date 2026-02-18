37c25050322da2c8269d557b11da7d47d6523e8b
chehin <chehinjuan@gmail.com>
2026-02-18 17:44:40 -0300

    orden sc

--- resources/views/admin/clientes/partials/form.blade.php
+++ resources/views/admin/clientes/partials/form.blade.php
@@ -1,4 +1,14 @@
+{{-- FILA 1 --}}
 <div class="row">
+    <div class="col-md-6">
+        <label>Razón Social</label>
+        <input type="text"
+               name="razon_social"
+               class="form-control"
+               value="{{ old('razon_social', $orden->razon_social ?? '') }}"
+               required>
+    </div>
+
     <div class="col-md-3">
         <label>CUIT</label>
         <input type="text"
@@ -9,25 +19,18 @@
                required>
     </div>
 
-    <div class="col-md-5">
-        <label>Razón Social</label>
+    <div class="col-md-3">
+        <label>Dirección (Localidad - Provincia)</label>
         <input type="text"
-               name="razon_social"
+               name="domicilio_comercial"
                class="form-control"
-               value="{{ old('razon_social', $orden->razon_social ?? '') }}"
+               value="{{ old('domicilio_comercial', $orden->domicilio_comercial ?? '') }}"
                required>
-    </div>
-
-    <div class="col-md-4">
-        <label>Email</label>
-        <input type="email"
-               name="email"
-               class="form-control"
-               value="{{ old('email', $orden->email ?? '') }}">
     </div>
 </div>
 
-{{-- NUEVA FILA --}}
+
+{{-- FILA 2 --}}
 <div class="row mt-3">
 
     <div class="col-md-4">
@@ -36,6 +39,14 @@
                name="telefono"
                class="form-control"
                value="{{ old('telefono', $orden->telefono ?? '') }}">
+    </div>
+
+    <div class="col-md-4">
+        <label>Email</label>
+        <input type="email"
+               name="email"
+               class="form-control"
+               value="{{ old('email', $orden->email ?? '') }}">
     </div>
 
     <div class="col-md-4">
@@ -59,10 +70,13 @@
 
 </div>
 
+
+{{-- FILA 3 --}}
 <div class="row mt-3">
+
     <div class="col-md-4">
         <div class="form-group">
-            <label for="condicion_arca">Condición ARCA</label>
+            <label for="condicion_arca">Condición IVA</label>
             <select name="condicion_arca" id="condicion_arca" class="form-control" required>
                 <option value="">Seleccione...</option>
 
@@ -106,12 +120,4 @@
         </div>
     </div>
 
-    <div class="col-md-4">
-        <label>Dirección (localidad - provincia)</label>
-        <input type="text"
-               name="domicilio_comercial"
-               class="form-control"
-               value="{{ old('domicilio_comercial', $orden->domicilio_comercial ?? '') }}"
-               required>
-    </div>
 </div>
