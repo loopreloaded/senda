a8c6f14193ff399decbdd16dbd1ad119fb32983a
juanchehin <chehinjuan@gmail.com>
2026-02-20 20:29:32 -0300

    orden alta SC

--- resources/views/admin/clientes/partials/form.blade.php
+++ resources/views/admin/clientes/partials/form.blade.php
@@ -1,6 +1,7 @@
 {{-- FILA 1 --}}
 <div class="row">
-    <div class="col-md-6">
+
+    <div class="col-md-8">
         <label>Razón Social</label>
         <input type="text"
                name="razon_social"
@@ -9,7 +10,7 @@
                required>
     </div>
 
-    <div class="col-md-3">
+    <div class="col-md-4">
         <label>CUIT</label>
         <input type="text"
                name="cuit"
@@ -19,7 +20,13 @@
                required>
     </div>
 
-    <div class="col-md-3">
+</div>
+
+
+{{-- FILA 2 --}}
+<div class="row mt-3">
+
+    <div class="col-md-4">
         <label>Dirección (Localidad - Provincia)</label>
         <input type="text"
                name="domicilio_comercial"
@@ -27,11 +34,6 @@
                value="{{ old('domicilio_comercial', $orden->domicilio_comercial ?? '') }}"
                required>
     </div>
-</div>
-
-
-{{-- FILA 2 --}}
-<div class="row mt-3">
 
     <div class="col-md-4">
         <label>Teléfono</label>
@@ -47,6 +49,54 @@
                name="email"
                class="form-control"
                value="{{ old('email', $orden->email ?? '') }}">
+    </div>
+
+</div>
+
+
+{{-- FILA 3 --}}
+<div class="row mt-3">
+
+    <div class="col-md-4">
+        <label>Condición IVA</label>
+        <select name="condicion_arca" class="form-control" required>
+            <option value="">Seleccione...</option>
+
+            <option value="RI" {{ old('condicion_arca', $orden->condicion_arca ?? '') == 'RI' ? 'selected' : '' }}>
+                Responsable Inscripto
+            </option>
+
+            <option value="EX" {{ old('condicion_arca', $orden->condicion_arca ?? '') == 'EX' ? 'selected' : '' }}>
+                Exento
+            </option>
+
+            <option value="NR" {{ old('condicion_arca', $orden->condicion_arca ?? '') == 'NR' ? 'selected' : '' }}>
+                No Responsable
+            </option>
+
+            <option value="CF" {{ old('condicion_arca', $orden->condicion_arca ?? '') == 'CF' ? 'selected' : '' }}>
+                Consumidor Final
+            </option>
+
+            <option value="MT" {{ old('condicion_arca', $orden->condicion_arca ?? '') == 'MT' ? 'selected' : '' }}>
+                Responsable Monotributo
+            </option>
+        </select>
+    </div>
+
+    <div class="col-md-4">
+        <label>Condición IIBB</label>
+        <select name="condicion_iibb" class="form-control" required>
+            <option value="">Seleccione...</option>
+
+            <option value="L" {{ old('condicion_iibb', $orden->condicion_iibb ?? '') == 'L' ? 'selected' : '' }}>
+                Local
+            </option>
+
+            <option value="CM" {{ old('condicion_iibb', $orden->condicion_iibb ?? '') == 'CM' ? 'selected' : '' }}>
+                Convenio Multilateral
+            </option>
+        </select>
     </div>
 
     <div class="col-md-4">
@@ -66,58 +116,6 @@
                 Ambos
             </option>
         </select>
-    </div>
-
-</div>
-
-
-{{-- FILA 3 --}}
-<div class="row mt-3">
-
-    <div class="col-md-4">
-        <div class="form-group">
-            <label for="condicion_arca">Condición IVA</label>
-            <select name="condicion_arca" id="condicion_arca" class="form-control" required>
-                <option value="">Seleccione...</option>
-
-                <option value="RI" {{ old('condicion_arca', $orden->condicion_arca ?? '') == 'RI' ? 'selected' : '' }}>
-                    Responsable Inscripto
-                </option>
-
-                <option value="EX" {{ old('condicion_arca', $orden->condicion_arca ?? '') == 'EX' ? 'selected' : '' }}>
-                    Exento
-                </option>
-
-                <option value="NR" {{ old('condicion_arca', $orden->condicion_arca ?? '') == 'NR' ? 'selected' : '' }}>
-                    No Responsable
-                </option>
-
-                <option value="CF" {{ old('condicion_arca', $orden->condicion_arca ?? '') == 'CF' ? 'selected' : '' }}>
-                    Consumidor Final
-                </option>
-
-                <option value="MT" {{ old('condicion_arca', $orden->condicion_arca ?? '') == 'MT' ? 'selected' : '' }}>
-                    Responsable Monotributo
-                </option>
-            </select>
-        </div>
-    </div>
-
-    <div class="col-md-4">
-        <div class="form-group">
-            <label for="condicion_iibb">Condición IIBB</label>
-            <select name="condicion_iibb" id="condicion_iibb" class="form-control" required>
-                <option value="">Seleccione...</option>
-
-                <option value="L" {{ old('condicion_iibb', $orden->condicion_iibb ?? '') == 'L' ? 'selected' : '' }}>
-                    Local
-                </option>
-
-                <option value="CM" {{ old('condicion_iibb', $orden->condicion_iibb ?? '') == 'CM' ? 'selected' : '' }}>
-                    Convenio Multilateral
-                </option>
-            </select>
-        </div>
     </div>
 
 </div>
